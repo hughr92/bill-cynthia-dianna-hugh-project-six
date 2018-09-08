1b1087554acb54c76f709725bd518c660e1e9929
Bill <skeche@gmail.com>
2018-09-08 18:50:21 -0400

    edited template arrays

---
+++ src/components/Templates_array.js
@@ -0,0 +1,66 @@
+const templates = {
+    cool: {
+    //one
+    coolOne: {
+        background: {
+        image: false,
+        color: ""
+        },
+        text: {
+            h2: {
+                content: "",
+                size: null,
+                fontFamily: "",
+                color: "",
+                alignment: ""
+            },
+            h1: {
+                size: null,
+                fontFamily: "",
+                color: "",
+                alignment: ""
+            }
+        }
+    },
+    //two
+    coolTwo: {
+        background: {
+        image: false,
+        color: ""
+        },
+        text: {
+        h2: {
+            size: null,
+            fontFamily: "",
+            color: "",
+            alignment: ""
+        },
+        h1: {
+            size: null,
+            fontFamily: "",
+            color: "",
+            alignment: ""
+        },
+        //three
+        coolThree: {
+            background: {
+            image: false,
+            color: ""
+            },
+            text: {
+            h2: {
+                size: null,
+                fontFamily: "",
+                color: "",
+                alignment: ""
+            },
+            h1: {
+                size: null,
+                fontFamily: "",
+                color: "",
+                alignment: ""
+            }
+        }
+    }
+    }
+    }
