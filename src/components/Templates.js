71cb3e85510e6db7c54ffd1fb13012efabd5ff35
Bill <skeche@gmail.com>
2018-09-09 15:48:26 -0400

    added to the array

--- src/components/Templates.js
+++ src/components/Templates.js
@@ -5,14 +5,28 @@ class Templates extends Component {
     render() {
         return (
             <div className="LandingPage">
+                <div className="wrapper">
                 <h1>TEMPLATES PAGE</h1>
 
-                <div>
-                    <h2>Template Title</h2>
+                <div className="styleGuide">
+                    <div className="styleGuide__cell">
+                        <h3 className="styleGuide__cell__heading">
+                        Template Title
+                        </h3>
+
+                        <div className="styleGuide__content">
+                        
+                        </div>
+                    </div>
                 </div>
 
-                <div className="styleGuide"> 
                 </div>
+
+                
+
+
+
+
 
                 
 
