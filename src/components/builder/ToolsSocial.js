f8434fcfae20d326c84dcac1e4c10f066c9460bc
Cynthia <cynthia.chiu.07@gmail.com>
2018-09-11 14:59:52 -0400

    socials reading from firebase

--- src/components/builder/ToolsSocial.js
+++ src/components/builder/ToolsSocial.js
@@ -44,8 +44,9 @@ class ToolsSocial extends Component {
         this.setState ({
             [e.target.id] : e.target.value
         }, ()=> {
+            const userID = this.props.user;
             console.log('social state',this.state);
-            const dbRef = firebase.database().ref(this.state.user)
+            const dbRef = firebase.database().ref(userID)
             console.log(`toolsocial`, dbRef);
             
             dbRef.on('value', snapshot => {
@@ -56,8 +57,8 @@ class ToolsSocial extends Component {
     }
 
     render() {
-
-        return  <div className="tools tools__container--social__selection">
+        console.log('render fire')
+        return  (<div className="tools tools__container--social__selection">
                 <h3>Enter Your Social Media Info</h3>
 
                     <form className="socialForm" onSubmit={this.handleSubmit}>
@@ -66,14 +67,14 @@ class ToolsSocial extends Component {
                         <input className="socialForm--input" onChange={this.handleChange} type="email" name="social" placeholder="Enter email" id="email" value={this.state.email}/>
 
                         <label for="socialTwitter">Twitter</label>
-                        <input className="socialForm--input" onChange={this.handleChange} type="text" placeholder="Enter Twitter username" maxLength="20" id="twitter"/>
+                        <input className="socialForm--input" onChange={this.handleChange} type="text" placeholder="Enter Twitter username" maxLength="20" id="twitter" value={this.state.twitter}/>
                     </div>
                     <div className="socialForm--half">
                         <label for="socialGithub">Github</label>
-                        <input className="socialForm--input" onChange={this.handleChange} type="text" placeholder="Enter Github username" maxLength="20" id="github"/>
+                        <input className="socialForm--input" onChange={this.handleChange} id="github" type="text" placeholder="Enter Github username" maxLength="20" id="github" value={this.state.github}/>
 
                         <label for="socialLinkedin">Linkedin</label>
-                        <input className="socialForm--input" onChange={this.handleChange} type="text" placeholder="Enter Linkedin username" id="linkedin" maxLength="20" />
+                        <input className="socialForm--input" onChange={this.handleChange} id="linkedin" type="text" placeholder="Enter Linkedin username" id="linkedin" value={this.state.linkedin} maxLength="20" />
                     </div>
 
 
@@ -82,7 +83,7 @@ class ToolsSocial extends Component {
                     </form>
                 </div>
             
-        
+        )
     }
 }
 
