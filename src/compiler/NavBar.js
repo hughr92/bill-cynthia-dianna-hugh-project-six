ac7931d130ea32b3a6f1274a1640101d40fd8a15
Dianna <hello@thisisdianna.com>
2018-09-11 14:56:21 -0400

    with Hugh's updates

--- src/compiler/NavBar.js
+++ src/compiler/NavBar.js
@@ -4,14 +4,14 @@ import { BrowserRouter as Router, Link, NavLink, Route } from 'react-router-dom'
 class NavBar extends Component {
     render() {
         return (
-
+            // {}
             // ternary statement - if not logged in show "login", else show logout
             <div className="navBar">
                 <div className="wrapper">
                     <nav>
                         <ul>
                             <li><NavLink to="/home">Home</NavLink></li>
-                            <li>Sign In</li>
+                            <li>Logout</li>
 
                         </ul>
                     </nav>
