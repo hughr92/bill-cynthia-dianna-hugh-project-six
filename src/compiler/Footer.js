f573f26b6b4f4094b8bdb2d1c154dfe330b01566
Hugh <hugh.m.rowland@gmail.com>
2018-09-11 12:38:39 -0400

    added styling, links to footer, created mixin for link - need to adjust the color, it is currently set to yellow

--- src/compiler/Footer.js
+++ src/compiler/Footer.js
@@ -1,73 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, NavLink, Route } from 'react-router-dom'
 
 class Footer extends Component {
     render() {
 
-        return <div>
-            <footer className="footer">Footer</footer>
-            <div className="wrapper">
-              <div className="footerContent">
-                <div class="footerContent__cell">
-                  <h3 class="footerContent__cell__heading">About Us</h3>
-                  <p class="footerContent__cell__body">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat.
-                  </p>
-                </div>
-                <div class="footerSocial">
-                  <div class="footerSocial__cell">
-                    <ul>
-                      <li class="facebook">
-                        <a class="socialLinks" href="#">
-                          <i class="fab fa-facebook-square" />
-                        </a>
-                      </li>
-                      <li class="instagram">
-                        <a class="socialLinks" href="#">
-                          <i class="fab fa-instagram" />
-                        </a>
-                      </li>
-                      <li class="pinterest">
-                        <a class="socialLinks" href="#">
-                          <i class="fab fa-pinterest-p" />
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
+        return (
 
-                  <nav class="footerNav">
-                    <h2 class="footerNav__heading">Splash Pad</h2>
-                    <ul class="navList nav__wrapped">
-                      <li class="footerNavList__item">
-                        <a href="#" class="navList__link">
-                          Home
-                        </a>
-                      </li>
-                      
-                      <li class="footerNavList__item">
-                        <a href="#" class="navList__link">
-                          Enquiries & Comments
-                        </a>
-                      </li>
-                      <li class="footerNavList__item">
-                        <a href="#" class="navList__link">
-                          About Us
-                        </a>
-                      </li>
-                    </ul>
-                  </nav>
-                  <p class="footer__copyright">
-                    © 2018 HappyCampers All rights reserved (Created by
-                    Bill, Cynthia, Dianna, Hugh)
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>;
+            <footer className="footer">
+                  <div className="wrapper">
+                    <p class="footer__copyright">
+                      &copy; 2018 HappyCampers (Created by: <a href="https://github.com/b-chuang"><i class="fab fa-github"></i> Bill</a>, <a href="https://github.com/cchiu01"><i class="fab fa-github"></i> Cynthia</a>, <a href="https://github.com/diannakylee"><i class="fab fa-github"></i> Dianna</a>, <a href="https://github.com/hughr92"><i class="fab fa-github"></i> Hugh</a>)
+                    </p>
+                  </div>
+            
+            </footer>
+        )
+            
+         
 
     }
 }
